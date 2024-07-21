2d2d3bc1ac4bc5e0b7d2dbd77eba076c37a59111
Praveen Kumar Thakur <pravee1701@gmail.com>
2024-07-21 15:44:02 +0530

    utils code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse{
+    constructor(statuCode, data, message = "Success"){
+        this.statuCode = statuCode;
+        this.data = data;
+        this.message = message;
+        this.success = statuCode < 400
+    }
+}
+
+export { ApiResponse }
