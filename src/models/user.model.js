639a7b869856192cdcd4c33712dddbd9a716ca59
Praveen Kumar Thakur <pravee1701@gmail.com>
2024-07-22 09:40:51 +0530

    multer and cloudinary setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,7 +53,7 @@ const userSchema = new Schema(
 )
 
 userSchema.pre("save", async function (next) {
-    if(!this.isModified("passwrod")) return next();
+    if(!this.isModified("password")) return next();
     this.passwrod = bcrypt.hash(this.passwrod, 10)
     next()
 })
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import { loadEnvFile } from "process";
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret:  process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadONCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath)  return null
+
+        const response = await cloudinary.uploader.upload(localFilePath,{
+
+            resource_type: "auto"
+        })
+
+        console.log("file is uploaded on cloudinary", response.url);
+
+        return response;
+    } catch (error) {
+        // remove the loally saved temporary file as the upload operation got failed
+
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+
+export { uploadONCloudinary }
